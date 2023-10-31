a35a3fa7a3bb29ae5d1b6c82e17dfcd61b5f12f6
adnannshaikh <101106625+adnannshaikh@users.noreply.github.com>
2023-10-31 12:17:57 +0530

    Add files via upload

---
+++ variableLearning.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+//	int x = 10;
+//	float num1 = 99.999;
+//	double num2 = 99.999999999999;
+//	//bool boolValue = true;
+//	
+//	char word = 't';
+
+	enum newlst {red,yellow,green};
+	enum newlst colorscm;
+	colorscm = green;
+//	printf("value:%c",word);
+}
