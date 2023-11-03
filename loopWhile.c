7a2f5ee718c008ce14b1dc63a9035caaa2da1da7
adnannshaikh <101106625+adnannshaikh@users.noreply.github.com>
2023-11-03 20:01:01 +0530

    Add files via upload

---
+++ loopWhile.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	int num = 0;
+//	printf("Enter a number: ");
+//	scanf("%i",&num);
+//	
+//	while(num!=5){
+//		printf("Enter a number: ");
+//		scanf("%i",&num);	
+//		
+//	}
+
+do{
+	printf("Enter a number: ");
+	scanf("%i",&num);
+	}while(num!=5);
+	
+}
